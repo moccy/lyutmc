69ceb81a47075b9ed4c8bdde5a00ad587207d9cb
Elliot Dewhurst <elliot+github@dewhurst.me>
2023-07-04 23:14:19 +0100

    split code in to functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::fs;
+
 use log::info;
-use wgpu::{self, DeviceDescriptor, Features, PipelineLayoutDescriptor, RenderPipelineDescriptor};
+use wgpu::{
+    self, Device, DeviceDescriptor, Features, PipelineLayoutDescriptor, RenderPipelineDescriptor,
+    Surface, SurfaceConfiguration, TextureFormat,
+};
 use winit::{
     dpi::{LogicalSize, PhysicalSize},
     event::{Event, WindowEvent},
-    event_loop::EventLoop,
+    event_loop::{ControlFlow, EventLoop},
     window::{Theme, Window, WindowBuilder},
 };
 
@@ -27,78 +32,19 @@ fn main() {
 
 async fn run(window: Window, event_loop: EventLoop<()>) {
     let instance = wgpu::Instance::default();
-    let surface = unsafe {
-        instance
-            .create_surface(&window)
-            .expect("Failed to create surface.")
-    };
+    let surface = create_surface(&instance, &window);
+    let adapter = create_adapter(instance, &surface).await;
+    let (device, queue) = create_device_and_queue(&adapter).await;
+    let shader = create_shader("src/shaders/triangle.wgsl", &device);
 
-    let adapter = instance
-        .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::HighPerformance,
-            force_fallback_adapter: false,
-            compatible_surface: Some(&surface),
-        })
-        .await
-        .expect("Failed to find an adapter.");
-
-    let (device, queue) = adapter
-        .request_device(
-            &DeviceDescriptor {
-                label: None,
-                features: Features::empty(),
-                limits: wgpu::Limits::downlevel_webgl2_defaults()
-                    .using_resolution(adapter.limits()),
-            },
-            None,
-        )
-        .await
-        .expect("Failed to create virtual device and queue.");
-
-    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: None,
-        source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(include_str!(
-            "shaders/triangle.wgsl"
-        ))),
-    });
-
-    let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
-        label: None,
-        bind_group_layouts: &[],
-        push_constant_ranges: &[],
-    });
+    let pipeline_layout = create_pipeline_layout(&device);
 
     let swapchain_capabilities = surface.get_capabilities(&adapter);
     let swapchain_format = swapchain_capabilities.formats[0];
 
-    let render_pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
-        label: None,
-        layout: Some(&pipeline_layout),
-        vertex: wgpu::VertexState {
-            module: &shader,
-            entry_point: "vs_main",
-            buffers: &[],
-        },
-        fragment: Some(wgpu::FragmentState {
-            module: &shader,
-            entry_point: "fs_main",
-            targets: &[Some(swapchain_format.into())],
-        }),
-        primitive: wgpu::PrimitiveState::default(),
-        depth_stencil: None,
-        multisample: wgpu::MultisampleState::default(),
-        multiview: None,
-    });
-
-    let mut config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: swapchain_format,
-        width: window.inner_size().width,
-        height: window.inner_size().height,
-        present_mode: wgpu::PresentMode::Fifo,
-        alpha_mode: swapchain_capabilities.alpha_modes[0],
-        view_formats: vec![],
-    };
+    let render_pipeline =
+        create_render_pipeline(&device, pipeline_layout, shader, swapchain_format);
+    let mut config = create_surface_config(swapchain_format, &window, swapchain_capabilities);
 
     surface.configure(&device, &config);
 
@@ -106,20 +52,9 @@ async fn run(window: Window, event_loop: EventLoop<()>) {
         control_flow.set_poll();
 
         match event {
-            Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
-                WindowEvent::CloseRequested => control_flow.set_exit(),
-                WindowEvent::Resized(dimensions) => {
-                    config.width = dimensions.width;
-                    config.height = dimensions.height;
-                    surface.configure(&device, &config);
-                }
-                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                    config.width = new_inner_size.width;
-                    config.height = new_inner_size.height;
-                    surface.configure(&device, &config);
-                }
-                _ => (),
-            },
+            Event::WindowEvent { window_id, event } if window_id == window.id() => {
+                handle_window_event(event, &window, &device, control_flow, &surface, &mut config)
+            }
             Event::MainEventsCleared => {
                 // At this point, all input events have been processed.
                 // Time to render the scene.
@@ -159,6 +94,142 @@ async fn run(window: Window, event_loop: EventLoop<()>) {
             _ => (),
         }
     });
+}
+
+fn handle_window_event(
+    event: WindowEvent<'_>,
+    window: &Window,
+    device: &Device,
+    control_flow: &mut ControlFlow,
+    surface: &Surface,
+    config: &mut SurfaceConfiguration,
+) {
+    match event {
+        WindowEvent::CloseRequested => control_flow.set_exit(),
+        WindowEvent::Resized(dimensions) => {
+            config.width = dimensions.width;
+            config.height = dimensions.height;
+            surface.configure(&device, &config);
+        }
+        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+            config.width = new_inner_size.width;
+            config.height = new_inner_size.height;
+            surface.configure(&device, &config);
+        }
+        WindowEvent::KeyboardInput { input, .. } => {
+            if input.state == winit::event::ElementState::Released {
+                match input.virtual_keycode {
+                    Some(winit::event::VirtualKeyCode::F11) => {
+                        toggle_fullscreen(&window);
+                    }
+                    _ => (),
+                }
+            }
+        }
+        _ => (),
+    }
+}
+
+fn create_surface_config(
+    swapchain_format: wgpu::TextureFormat,
+    window: &Window,
+    swapchain_capabilities: wgpu::SurfaceCapabilities,
+) -> wgpu::SurfaceConfiguration {
+    wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: swapchain_format,
+        width: window.inner_size().width,
+        height: window.inner_size().height,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: swapchain_capabilities.alpha_modes[0],
+        view_formats: vec![],
+    }
+}
+
+fn create_render_pipeline(
+    device: &wgpu::Device,
+    pipeline_layout: wgpu::PipelineLayout,
+    shader: wgpu::ShaderModule,
+    swapchain_format: wgpu::TextureFormat,
+) -> wgpu::RenderPipeline {
+    device.create_render_pipeline(&RenderPipelineDescriptor {
+        label: None,
+        layout: Some(&pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vs_main",
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "fs_main",
+            targets: &[Some(swapchain_format.into())],
+        }),
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview: None,
+    })
+}
+
+fn create_pipeline_layout(device: &wgpu::Device) -> wgpu::PipelineLayout {
+    device.create_pipeline_layout(&PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    })
+}
+
+fn create_surface(instance: &wgpu::Instance, window: &Window) -> wgpu::Surface {
+    unsafe {
+        instance
+            .create_surface(window)
+            .expect("Failed to create surface.")
+    }
+}
+
+async fn create_adapter(instance: wgpu::Instance, surface: &wgpu::Surface) -> wgpu::Adapter {
+    instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(surface),
+        })
+        .await
+        .expect("Failed to find an adapter.")
+}
+
+async fn create_device_and_queue(adapter: &wgpu::Adapter) -> (wgpu::Device, wgpu::Queue) {
+    adapter
+        .request_device(
+            &DeviceDescriptor {
+                label: None,
+                features: Features::empty(),
+                limits: wgpu::Limits::downlevel_webgl2_defaults()
+                    .using_resolution(adapter.limits()),
+            },
+            None,
+        )
+        .await
+        .expect("Failed to create virtual device and queue.")
+}
+
+fn create_shader(shader_path: &str, device: &wgpu::Device) -> wgpu::ShaderModule {
+    let shader_source = fs::read_to_string(shader_path)
+        .expect(format!("Failed to load shader at path: {}", shader_path).as_str());
+
+    device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(shader_source.into()),
+    })
+}
+
+fn toggle_fullscreen(window: &Window) {
+    if window.fullscreen().is_none() {
+        window.set_fullscreen(Some(winit::window::Fullscreen::Borderless(None)))
+    } else {
+        window.set_fullscreen(None);
+    }
 }
 
 fn get_window_sizes(
